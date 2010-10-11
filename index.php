895bd29540e676db8f610b50287edb5276ff44f6
riotorto <riotorto@3d521886-4347-4e5a-b8a1-810c3ab1592d>
2010-10-11 14:06:32 +0000

    Index script
    git-svn-id: https://svn.code.sf.net/p/yamwi/code@6 3d521886-4347-4e5a-b8a1-810c3ab1592d

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+<title>Maxima on line</title>
+</head>
+
+<body>
+
+<p align="right"><font size="-1">Help:
+<a href="help/help_en.html" target="blank">English</a>,
+<a href="help/help_es.html" target="blank">Espa&ntilde;ol</a>,
+<a href="help/help_gl.html" target="blank">Galego</a>
+</font></p>
+
+<center><font size="+3" color="blue"><i>Yamwi</i></font></center>
+<hr>
+
+<?php
+include('yamwi.php');
+?>
+<hr>
+<font size="-1"><i><a href="http://yamwi.sourceforge.net" target = "_blank">Yamwi, (Yet Another Maxima Web Interface)</a></i></font>
+</body>
+</html>
+
