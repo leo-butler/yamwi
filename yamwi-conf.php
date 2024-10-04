6c15f5818e94677e30633604df5a8a5d45ed2401
Leo Butler <leo.butler@umanitoba.ca>
2024-10-04 16:34:34 -0500

    yawmi.php: move configuration to yamwi-conf.php

---
+++ yamwi-conf.php
@@ -0,0 +1,34 @@
+<?php
+$mode = 2; // 0 -> ASCII-Art output,
+           // 1 -> Binary TeX output,
+           // 2 -> Enhanced ASCII-Art output,
+           // 3 -> Syntactic output
+           // 4 -> Remote TeX + MathJax
+	       // 5 -> MathML
+
+$max_file_time = 1;
+
+$max_process_time = 120;
+
+$max_num_processes = 30;
+
+$timelimit_binary = '/usr/bin/timeout';
+
+$maxima_binary = '/usr/bin/maxima';
+
+$maxima_args = "";
+
+
+//////////////
+// MESSAGES //
+//////////////
+
+
+$message_dangerous = "Yamwi detected forbidden code: ";
+$message_time_process = "Requested process aborted. It exceeded maximum execution time.";
+$message_too_many_processes = "Too many users. Please, try later.";
+$message_prog_error = "Programming error detected. Check your input.";
+$submit_button = "Submit";
+$clear_button = "Clear";
+
+?>
