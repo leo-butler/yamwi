6647aaed46985ed03a2d17045cb59d7e10836494
Leo Butler <leo.butler@umanitoba.ca>
2024-10-24 13:38:08 -0500

    yamwi-conf.php: provide short-circuit for mode=1
    Binary TeX output requires a working LaTeX installation plus texi2dvi
    and convert. Set this flag to true to avoid the need to install it.

--- yamwi-conf.php
+++ yamwi-conf.php
@@ -6,6 +6,8 @@ $mode = 2; // 0 -> ASCII-Art output,
            // 4 -> Remote TeX + MathJax
 	       // 5 -> MathML
 
+$mode1is4 = true;  // Set to true to dis-able Binary TeX output
+
 $max_file_time = 1;
 
 $max_process_time = 120;
