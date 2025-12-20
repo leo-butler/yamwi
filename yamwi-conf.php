ca8b8d8bd39db2d768b51b0465029a43a4fdf3d0
Leo Butler <leo.butler@umanitoba.ca>
2025-12-20 14:07:10 -0600

    yamwi-conf.php: add configuration variables for movie generation

--- yamwi-conf.php
+++ yamwi-conf.php
@@ -22,6 +22,14 @@ $maxima_args = "";
 
 $magic_key = "1dc53ea6b0ae1e618fc4e123238192b"; // CHANGE THIS!!
 
+$movie_muxer = 'webm'; // needs to be one of webm, mp4, ogg
+
+$movie_is_embedded = 1;
+
+$ffmpeg_bin = '/usr/bin/ffmpeg';
+
+$base64_cmd = '/usr/bin/base64 -w 0';
+
 //////////////
 // MESSAGES //
 //////////////
