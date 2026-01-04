eb61b63ffa4ac6693adcd50bf820c0b54ab4604c
Leo Butler <leo.butler@umanitoba.ca>
2026-01-03 22:31:25 -0600

    yamwi-conf.php: add gnuplot_command, comments about muxer
    In a current firefox, ogg does not work in either capacity.
    In a current ffmpeg, setting the output muxer to be mp4 and output to
    STDOUT produces an error (non-seekable stream).

--- yamwi-conf.php
+++ yamwi-conf.php
@@ -20,11 +20,13 @@ $maxima_binary = '/usr/bin/maxima';
 
 $maxima_args = "";
 
+$gnuplot_command = '/usr/bin/gnuplot';
+
 $magic_key = "1dc53ea6b0ae1e618fc4e123238192b"; // CHANGE THIS!!
 
-$movie_muxer = 'webm'; // needs to be one of webm, mp4, ogg
+$movie_muxer = 'webm'; // needs to be one of webm, mp4 (ogg does not work)
 
-$movie_is_embedded = 1;
+$movie_is_embedded = 0; // webm can be 0 or 1; mp4 must be 0.
 
 $ffmpeg_bin = '/usr/bin/ffmpeg';
 
