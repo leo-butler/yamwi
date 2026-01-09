46c2a646edfd0c51d9ae1ef05ad7fa002e0fdb75
Leo Butler <leo.butler@umanitoba.ca>
2026-01-09 15:39:03 -0600

    yawmi.lisp: patch RETRIEVE, remove ASKSIGN1
    RETRIEVE is the lisp function that Maxima uses to ask questions of the
    user.
    
    This patch overwrites RETRIEVE and makes it:
    
    - write the question, wrapped in a customized piece of html
    - throw to macsyma-quit, which stops evaluation of any further code
    
    This seems preferable to a blanket error.
    
    ----
    Note that ASKSIGN1 was not the right function to do this in, because
    it is bypassed by many questions.

--- yamwi.lisp
+++ yamwi.lisp
@@ -1,50 +1,16 @@
-;; 'asksign1' is redefined to avoid asking the user
-;; about the sign of constants. Instead of calling
-;; macro 'ask', we return an error message of type
-;; 'yamwi1', which will be later managed by the php script.
-(defun asksign1 ($askexp)
-  (let ($radexpand)
-    (declare (special $radexpand))
-    (sign1 $askexp))
-  (cond ((has-int-symbols $askexp)
-	 '$pnz)
-	((member sign '($pos $neg $zero $imaginary) :test #'eq) sign)
-	((null odds)
-	 (setq $askexp (lmul evens)
-	       sign (cdr (assol $askexp locals)))
-	 (do ()
-	     (nil)
-	   (cond ((member sign '($zero |$Z| |$z| 0 0.0) :test #'equal)
-		  (tdzero $askexp) (setq sign '$zero) (return t))
-		 ((member sign '($pn $nonzero |$N| |$n| $nz $nonz $non0) :test #'eq)
-		  (tdpn $askexp) (setq sign '$pos) (return t))
-		 ((member sign '($pos |$P| |$p| $positive) :test #'eq)
-		  (tdpos $askexp) (setq sign '$pos) (return t))
-		 ((member sign '($neg |$N| |$n| $negative) :test #'eq)
-		  (tdneg $askexp) (setq sign '$pos) (return t)))
-	   (merror "yamwi1a ~M yamwi1b" ($listofvars $askexp)))
-	 (if minus (flip sign) sign))
-	(t (if minus (setq sign (flip sign)))
-	   (setq $askexp (lmul (nconc odds (mapcar #'(lambda (l) (pow l 2)) evens))))
-	   (do ((dom (cond ((eq '$pz sign) "  positive or zero?")
-			   ((eq '$nz sign) "  negative or zero?")
-			   ((eq '$pn sign) "  positive or negative?")
-			   (t "  positive, negative, or zero?")))
-		(ans (cdr (assol $askexp locals))))
-	       (nil)
-	     (cond ((and (member ans '($pos |$P| |$p| $positive) :test #'eq)
-			 (member sign '($pz $pn $pnz) :test #'eq))
-		    (tdpos $askexp) (setq sign '$pos) (return t))
-		   ((and (member ans '($neg |$N| |$n| $negative) :test #'eq)
-			 (member sign '($nz $pn $pnz) :test #'eq))
-		    (tdneg $askexp) (setq sign '$neg) (return t))
-		   ((and (member ans '($zero |$Z| |$z| 0 0.0) :test #'equal)
-			 (member sign '($pz $nz $pnz) :test #'eq))
-		    (tdzero $askexp) (setq sign '$zero) (return t)))
-	     (merror "yamwi1a ~M yamwi1b" ($listofvars $askexp)))
-	   (if minus (flip sign) sign))))
-
-
+;; RETRIEVE is the function used to ask questions of the user.
+;; We redefine it here to return print the question and then throw to the tag macsyma-quit.
+;; This halts further batch-ing of the script, but allows previously computed results to be printed
+;; along with the question.
+(defun retrieve (form &rest optional)
+  (declare (ignore optional))
+  (let ((msg (with-output-to-string (s)
+	       (let ($display2d *alt-display1d* (*standard-output* s) *display-labels-p*)
+		 (format t "<tr class='retrieve'><td></td><td><span class='retrieve'>Maxima asked: <u>")
+		 (displa form)
+		 (format t "</u></span></td></tr>")))))
+    (displa (list '(mtext) (list '(mtext) msg)))
+    (throw 'macsyma-quit nil)))
 
 ;; to avoid problems with Internet browsers when reading strict ordering symbols
 ;; in inequalities (<, >) in LaTeX mode (mainly for MathJax), we need them
