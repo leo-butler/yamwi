021836ab387b63c54d6a774a26117a380df81975
Leo Butler <leo.butler@umanitoba.ca>
2026-03-25 11:36:58 -0500

    yamwi.lisp: Fix bug #12 Allow answers to be read by RETRIEVE
    This is a partial fix, as noted in that issue. We first allow the
    original RETRIEVE function to try to read an answer to a question. If
    it fails and throws to MACSYMA-QUIT, then we revert to the previous
    behaviour and write a message with the question in it and then
    re-throw to MACSYMA-QUIT in order to halt further computation.
    
    Ref: https://github.com/leo-butler/yamwi/issues/12

--- yamwi.lisp
+++ yamwi.lisp
@@ -5,15 +5,21 @@
 ;; We redefine it here to return print the question and then throw to the tag macsyma-quit.
 ;; This halts further batch-ing of the script, but allows previously computed results to be printed
 ;; along with the question.
+(defvar *retrieve-fun* (symbol-function 'retrieve))
 (defun retrieve (form &rest optional)
-  (declare (ignore optional))
   (let ((msg (with-output-to-string (s)
 	       (let ($display2d *alt-display1d* (*standard-output* s) *display-labels-p*)
 		 (format t "<tr class='retrieve'><td></td><td><span class='retrieve'>Maxima asked: <u>")
 		 (displa form)
-		 (format t "</u></span></td></tr>")))))
-    (displa (list '(mtext) (list '(mtext) msg)))
-    (throw 'macsyma-quit nil)))
+		 (format t "</u></span></td></tr><tr class='retrieve'><td></td><td><span class='retrieve'>Questions can be answered by using <a href='https://maxima.sourceforge.io/docs/manual/Maxima_0027s-Database.html#index-assume'>assume</a> or typing `y;', `n;', `p;', `z;', `nz;' as appropriate.")))))
+    (let ((result (catch 'macsyma-quit
+		    (let ((*error-output* (make-string-output-stream)))
+		      (apply *retrieve-fun* `(,form ,@optional))))))
+      (cond ((eq result 'maxima-error)
+	     (displa (list '(mtext) (list '(mtext) msg)))
+	     (throw 'macsyma-quit nil))
+	    (t
+	     result)))))
 
 ;; to avoid problems with Internet browsers when reading strict ordering symbols
 ;; in inequalities (<, >) in LaTeX mode (mainly for MathJax), we need them
